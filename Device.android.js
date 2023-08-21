28530d4fe25f66a721410bbd0803a8db5945635e
Trevize Daneel <comporell@users.noreply.github.com>
2023-08-21 23:01:03 +0300

    ES6 Module Export fix & Module not found error fix
    I have various errors with this plugin such as
    
    ERROR in ./node_modules/nativescript-wifi-settings/Device.android.js 2:16-38
    Module not found: Error: Can't resolve 'utils/types' in ...
    
    this should fix it but please check it first
    sorry for styling it :\

--- Device.android.js
+++ Device.android.js
@@ -1,41 +1,39 @@
 import * as application from "@nativescript/core"
 
 function getAppContext() {
-  var ctx = application.android.context;
+	var ctx = application.android.context;
 
-  if (ctx==null) {
-    ctx = java.lang.Class.forName("android.app.AppGlobals")
-      .getMethod("getInitialApplication", null)
-      .invoke(null, null);
-  }
+	if (ctx == null) {
+		ctx = java.lang.Class.forName("android.app.AppGlobals")
+			.getMethod("getInitialApplication", null)
+			.invoke(null, null);
+	}
 
-  if (ctx==null) {
-    ctx = java.lang.Class.forName("android.app.ActivityThread")
-      .getMethod("currentApplication", null)
-      .invoke(null, null);
-  }
+	if (ctx == null) {
+		ctx = java.lang.Class.forName("android.app.ActivityThread")
+			.getMethod("currentApplication", null)
+			.invoke(null, null);
+	}
 
-  if (ctx!==null) {
-    ctx = ctx.getApplicationContext();
-  } else {
-    ctx = undefined;
-  }
+	if (ctx !== null) {
+		ctx = ctx.getApplicationContext();
+	} else {
+		ctx = undefined;
+	}
 
-  return ctx;
+	return ctx;
 }
-module.exports = {
-  openWifiSettingsOnDevice: function() {
-    var ctx = getAppContext();
-    if (ctx==null) {
-      return false;
-    }
+export function openWifiSettingsOnDevice() {
+	var ctx = getAppContext();
+	if (ctx == null) {
+		return false;
+	}
 
-    var intent = new android.content.Intent(
-      android.provider.Settings.ACTION_WIFI_SETTINGS,
-    );
-    intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+	var intent = new android.content.Intent(
+		android.provider.Settings.ACTION_WIFI_SETTINGS,
+	);
+	intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
 
-    ctx.startActivity(intent);
-    return true;
-  },
+	ctx.startActivity(intent);
+	return true;
 };
--- Device.ios.js
+++ Device.ios.js
@@ -1,33 +1,31 @@
 import * as application from "@nativescript/core"
 
 function getAppContext() {
-  return application.ios.delegate;
+	return application.ios.delegate;
 }
 exports.getAppContext = getAppContext;
 
 function getAppView() {
-  return application.ios.rootController;
+	return application.ios.rootController;
 }
 exports.getAppView = getAppView;
 function openUri(uri) {
-  var u = NSURL.URLWithString(uri);
+	var u = NSURL.URLWithString(uri);
 
-  if (UIApplication.sharedApplication.canOpenURL(u)) {
-    return UIApplication.sharedApplication.openURL(u);
-  }
+	if (UIApplication.sharedApplication.canOpenURL(u)) {
+		return UIApplication.sharedApplication.openURL(u);
+	}
 
-  return false;
+	return false;
 }
 exports.openUri = openUri;
-module.exports = {
-  openWifiSettingsOnDevice: function() {
-    var uris = ["prefs:root=WIFI", "App-Prefs:root=WIFI"];
-    for (var i = 0; i < uris.length; i++) {
-      if (openUri(uris[i])) {
-        return true;
-      }
-    }
+export function openWifiSettingsOnDevice() {
+	var uris = ["prefs:root=WIFI", "App-Prefs:root=WIFI"];
+	for (var i = 0; i < uris.length; i++) {
+		if (openUri(uris[i])) {
+			return true;
+		}
+	}
 
-    return false;
-  },
+	return false;
 };
